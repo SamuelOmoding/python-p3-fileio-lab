4bcb06fc9510e729c3a0a0daeb2cfa45f16d7b95
Omoding-web <samuel.omoding@student.moringaschool.com>
2024-03-06 15:32:46 +0300

    Assignment done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,22 @@
 def write_file(file_name, file_content):
-    pass
+    #To open the file for writing
+    with open(str(file_name) + '.txt', 'w', encoding='utf-8') as f: 
+       #Write
+        f.write(file_content)
+        pass
 
 def append_file(file_name, append_content):
-    pass
+     # Open the file for appending
+     #File converted to string,opens in writing mode and encoding for reading
+     with open(str(file_name) + '.txt', 'a', encoding='utf-8') as f:
+       #Append the content
+        f.write(append_content)
+        pass
 
 def read_file(file_name):
-    pass
+    #Open the file for reading
+     with open(str(file_name) + '.txt', 'r', encoding='utf-8') as f:
+        #Read
+        file_content = f.read()
+     return file_content
+pass
